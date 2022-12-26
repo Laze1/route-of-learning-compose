e95b4f540ec4cdaab5a1aad679977c07cee9d2db
吴溪 <wux@vecentek.com>
2022-12-26 14:53:50 +0800

    状态重组

--- app/src/main/java/com/example/myapplicationcompose/learn/WaterCounter.kt
+++ app/src/main/java/com/example/myapplicationcompose/learn/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.myapplicationcompose.learn
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -13,13 +14,31 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        var count by remember{ mutableStateOf(0) }
-        Text(
-            text = "You've had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text(text = "Add one")
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
+            Text(text = "You've had $count glasses.")
+        }
+        Row(Modifier.padding(8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            //当count=0时，上方(count>0)内会重组，导致showTask记忆丢失,再次使用则为初始状态
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "clear count")
+            }
         }
     }
 }
