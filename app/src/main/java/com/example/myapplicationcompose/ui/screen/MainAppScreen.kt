7ed56925d70f2b00da6d5f596f034e0be2948dfb
吴溪 <546030736@qq.com>
2023-02-07 15:03:31 +0800

    topBar drawer

--- app/src/main/java/com/example/myapplicationcompose/ui/screen/MainAppScreen.kt
+++ app/src/main/java/com/example/myapplicationcompose/ui/screen/MainAppScreen.kt
@@ -1,6 +1,6 @@
 package com.example.myapplicationcompose.ui.screen
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
@@ -9,11 +9,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.myapplicationcompose.navigation.ComposeNavigation
 import com.example.myapplicationcompose.navigation.MainFragmentList
 import com.example.myapplicationcompose.navigation.bottomNavigationList
@@ -36,19 +38,43 @@ fun MainContent(
             color = MaterialTheme.colors.background
         ) {
             Scaffold(
-                modifier = Modifier.background(color = Color.Black),
-                topBar = {},
+                topBar = { TopBar() },
                 bottomBar = {
                     if (barShow) {
                         BottomBar { navController.navigateSingleTopTo(it) }
                     }
                 },
-                drawerContent = {},
+                drawerContent = if (barShow) { { DrawerContent()} } else null,
             ) { paddingValues ->
-                ComposeNavigation(navController, modifier = Modifier.padding(paddingValues)){
+                ComposeNavigation(navController, modifier = Modifier.padding(paddingValues)) {
                     barShow = it
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun DrawerContent() {
+    AsyncImage(
+        model = "https://img1.baidu.com/it/u=3353028008,1916857228&fm=253&fmt=auto&app=138&f=JPG?w=800&h=500",
+        contentDescription = null
+    )
+    Text(text = "这是一个侧边栏", modifier = Modifier.padding(15.dp))
+}
+
+@Composable
+private fun TopBar() {
+    TopAppBar(
+        modifier = Modifier.padding(0.dp),
+        backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colors.onBackground,
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "首页",
+                fontSize = androidx.compose.material3.MaterialTheme.typography.titleMedium.fontSize
+            )
         }
     }
 }
@@ -65,3 +91,4 @@ fun BottomBar(onclick: (String) -> Unit) {
         }
     }
 }
+
